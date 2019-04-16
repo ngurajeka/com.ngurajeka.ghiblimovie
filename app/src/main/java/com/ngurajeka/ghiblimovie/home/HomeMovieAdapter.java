4be0ee019e681fbd650090f582df29990c3345cc
ngurajeka <ngurajeka.92@gmail.com>
2019-04-16 09:18:58 +0700

    implement glide for image processing

--- app/src/main/java/com/ngurajeka/ghiblimovie/home/HomeMovieAdapter.java
+++ app/src/main/java/com/ngurajeka/ghiblimovie/home/HomeMovieAdapter.java
@@ -3,20 +3,31 @@ package com.ngurajeka.ghiblimovie.home;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.ngurajeka.ghiblimovie.R;
 import com.ngurajeka.ghiblimovie.model.Film;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+@SuppressWarnings("WeakerAccess")
 public class HomeMovieAdapter extends RecyclerView.Adapter<HomeMovieAdapter.ViewHolder> {
 
     private List<Film> films;
     private HomeFragment.FilmItemListener mListener;
+
+    private View rootView;
+
+    private final Map<String, String> thumbnails = new HashMap<String, String>() {{
+        put("58611129-2dbc-4a81-a72f-77ddfc1b1b49", "https://upload.wikimedia.org/wikipedia/en/0/02/My_Neighbor_Totoro_-_Tonari_no_Totoro_%28Movie_Poster%29.jpg");
+    }};
 
     HomeMovieAdapter(List<Film> films, HomeFragment.FilmItemListener mListener) {
         this.films = films;
@@ -26,15 +37,17 @@ public class HomeMovieAdapter extends RecyclerView.Adapter<HomeMovieAdapter.View
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_film_home, parent, false);
-        return new ViewHolder(v);
+        rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_film_home, parent, false);
+        return new ViewHolder(rootView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Film film = films.get(position);
+        String thumbnail = thumbnails.get(film.getId());
+
         holder.titleTxt.setText(film.getTitle());
-        holder.descriptionTxt.setText(film.getDescription());
+        if (thumbnail != null) Glide.with(rootView).load(thumbnail).into(holder.coverImg);
         holder.itemView.setOnClickListener(__ -> mListener.onFilmClick(film));
     }
 
@@ -51,13 +64,13 @@ public class HomeMovieAdapter extends RecyclerView.Adapter<HomeMovieAdapter.View
     class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView titleTxt;
-        TextView descriptionTxt;
+        ImageView coverImg;
 
         ViewHolder(View itemView) {
             super(itemView);
 
             titleTxt = itemView.findViewById(R.id.title);
-            descriptionTxt = itemView.findViewById(R.id.description);
+            coverImg = itemView.findViewById(R.id.cover);
         }
     }
 
